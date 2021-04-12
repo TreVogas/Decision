5804f18c77053fdfe1189c8d18d3318aaa7240d9
TreVogas <63095431+TreVogas@users.noreply.github.com>
2021-04-12 23:39:20 +0500

    Transcribing DNA into RNA

--- Decision/src/Transcribing_DNA_into_RNA.java
+++ Decision/src/Transcribing_DNA_into_RNA.java
@@ -1,7 +1,13 @@
 import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
+/*An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
 
+Given a DNA string t corresponding to a coding strand, its transcribed RNA string u is formed by replacing all occurrences of 'T' in t with 'U' in u.
+
+Given: A DNA string t having length at most 1000 nt.
+
+Return: The transcribed RNA string of t.*/
 public class Transcribing_DNA_into_RNA {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
