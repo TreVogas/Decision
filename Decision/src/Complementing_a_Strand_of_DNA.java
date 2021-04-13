f97be67348b18cd9f6f140d34c7eee904a6fc95f
TreVogas <63095431+TreVogas@users.noreply.github.com>
2021-04-13 18:58:09 +0500

    Complementing a Strand of DNA

--- Decision/src/Complementing_a_Strand_of_DNA.java
+++ Decision/src/Complementing_a_Strand_of_DNA.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 
-public class Complementing_a_Strand_of_DNA  {
+/*
+Problem
+In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
+
+The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s, then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
+
+Given: A DNA string s of length at most 1000 bp.
+ */
+
+public class Complementing_a_Strand_of_DNA {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String Input = scanner.nextLine();
         char[] Char_line = Input.toCharArray();
         char[] Char_Invert = new char[Char_line.length];
-        for (int i =0;i<Char_Invert.length;i++){
-            Char_Invert[i] = Char_line[i];
-        }
+        System.arraycopy(Char_line, 0, Char_Invert, 0, Char_Invert.length);
         for(int i=0;i<Char_line.length;i++){
             Char_Invert[i] = Char_line[Char_Invert.length-i-1];
         }
