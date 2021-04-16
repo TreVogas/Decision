0f5f2dea55e2b4b77bbd1c906e3164296dbff203
TreVogas <63095431+TreVogas@users.noreply.github.com>
2021-04-16 23:07:58 +0500

    Decision

---
+++ Decision/src/Mendels_First_Law.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/*
+Problem
+
+Figure 2. The probability of any outcome (leaf) in a probability tree diagram is given by the product of probabilities from the start of the tree to the outcome. For example, the probability that X is blue and Y is blue is equal to (2/5)(1/4), or 1/10.
+Probability is the mathematical study of randomly occurring phenomena. We will model such a phenomenon with a random variable, which is simply a variable that can take a number of different distinct outcomes depending on the result of an underlying random process.
+
+For example, say that we have a bag containing 3 red balls and 2 blue balls. If we let X represent the random variable corresponding to the color of a drawn ball, then the probability of each of the two outcomes is given by Pr(X=red)=35 and Pr(X=blue)=25.
+
+Random variables can be combined to yield new random variables. Returning to the ball example, let Y model the color of a second ball drawn from the bag (without replacing the first ball). The probability of Y being red depends on whether the first ball was red or blue. To represent all outcomes of X and Y, we therefore use a probability tree diagram. This branching diagram represents all possible individual probabilities for X and Y, with outcomes at the endpoints ("leaves") of the tree. The probability of any outcome is given by the product of probabilities along the path from the beginning of the tree; see Figure 2 for an illustrative example.
+
+An event is simply a collection of outcomes. Because outcomes are distinct, the probability of an event can be written as the sum of the probabilities of its constituent outcomes. For our colored ball example, let A be the event "Y is blue." Pr(A) is equal to the sum of the probabilities of two different outcomes: Pr(X=blue and Y=blue)+Pr(X=red and Y=blue), or 310+110=25
+ */
+public class Mendels_First_Law {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        float k = scanner.nextInt();
+        float m = scanner.nextInt();
+        float n = scanner.nextInt();
+        float s = k+m+n;
+        float result;
+        result =1 - (((m*(m-1))/(4*s*(s-1))) + ((m*n)/(s*(s-1)))+(n*(n-1)/(s*(s-1))));
+        System.out.println(result);
+
+    }
+}
